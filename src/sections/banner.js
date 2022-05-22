17864c8603bca931bf97f02f175b0c5d642ec2a1
billyaoun <99548401+billyaoun@users.noreply.github.com>
2022-05-23 02:44:34 +0300

    Update banner.js

--- src/sections/banner.js
+++ src/sections/banner.js
@@ -16,7 +16,11 @@ export default function Banner() {
           <Text as="p" variant="heroSecondary">
             There is...in the world
           </Text>
-          <Button variant="primary">Donate</Button>
+          <form action="https://www.paypal.com/donate" method="post" target="_top">
+            <input type="hidden" name="hosted_button_id" value="UCC8P7FG279KC" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/GB/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_GB/i/scr/pixel.gif" width="1" height="1" />
+          </form>
         </Box>
 
       </Container>
