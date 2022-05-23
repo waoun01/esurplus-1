8d6d8fefef1d33b552e7485be24fa307408e59be
billyaoun <99548401+billyaoun@users.noreply.github.com>
2022-05-23 17:25:04 +0300

    Update index.js

--- src/theme/index.js
+++ src/theme/index.js
@@ -12,7 +12,8 @@ export default {
     //primary: '#EA3A60', // primary button and link color
     //primary:'#9c1111',
     primary: "#006400",
-    secondary: '#9c1111', // secondary color - can be used for hover states
+    secondary: '#006400',
+    //secondary: '#9c1111', // secondary color - can be used for hover states
     muted: '#E4E4E4', // muted color
     accent: '#609', // a contrast color for emphasizing UI
 
