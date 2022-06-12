3dbd9c23a680e5e1fb9dc797d27f5643fde8d882
billyaoun <99548401+billyaoun@users.noreply.github.com>
2022-06-12 22:17:32 +0100

    Update team-section.js

--- src/sections/team-section.js
+++ src/sections/team-section.js
@@ -99,7 +99,7 @@ const data = [
     imgSrc: Member4,
     altText: 'Paul Meyer',
     title: 'Paul Meyer',
-    designation: 'Founder, Director, and Reseacher',
+    designation: 'Director, Reseacher, Stripper, Dancer, and Pet',
     socialProfile: [
       {
         id: 1,
